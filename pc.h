d61ddd688ceae91137d07fa03e1ac2071a78288c
FranWang123 <sscatishere@hotmail.com>
2017-03-26 16:42:32 -0400

    Add files via upload

---
+++ pc.h
@@ -0,0 +1,19 @@
+#ifndef _PC_H_
+#define _PC_H_
+#include <string>
+#include <iostream>
+#include "character.h"
+
+class PC: piblic Character {
+  int max;
+  public:
+  virtual PC(int initHP, int initAtk, int initDef, int Gold, int row, int col, int max);
+  virtual ~PC();
+  virtual attack(Enemy *target) = 0;
+  virtual void move(std::string direction);
+  virtual void getGold();
+  void pickUpItem(Item *i);
+  void getMax()
+};
+
+#endif
