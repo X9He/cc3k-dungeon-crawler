df6f959c0bd652f366cd3c710af9f3c729fbf196
FranWang123 <sscatishere@hotmail.com>
2017-03-26 15:48:58 -0400

    Add files via upload

---
+++ character.h
@@ -0,0 +1,33 @@
+#ifndef _CHARACTER_H_
+#define _CHARACTER_H_
+#include <string>
+#include <iostream>
+class Item;
+
+class Character{
+  int initHP, initAtk, initDef, Gold, row, col, curHp, curAtk, curDef;
+  char name;
+  publlic:
+  virtual void move() = 0;
+  virtual void move(string direction) = 0;
+  virtual void attack(Character &Target) = 0;
+  virtual void defense() = 0;
+  Character();
+  virtual  ~Character();
+  virtual void damage() = 0;
+  bool die();
+  virtual void changeHP(int effect) = 0;
+  virtual void changeAtk(int effect) = 0;
+  virtual void changeDef(int effect) = 0;
+  virtual int getHp() = 0;
+  virtual int getAtk() = 0;
+  virtual int getDef() = 0;
+  virtual int getGold() = 0;
+  virtual void changeGold() = 0;
+  int getrow();
+  int getcol();
+  char getName();
+  void initHPAtkDef(); 
+};  
+
+#endif
