d1e484e13096042a0d0a844b51d7249e1d9f820a
FranWang123 <sscatishere@hotmail.com>
2017-03-27 17:48:02 -0400

    Update enemy.cc

--- enemy.cc
+++ enemy.cc
@@ -8,6 +8,15 @@
 Enemy::Enemy(int initHP, int initAtk, int initDef, int Gold, int row, int col, PC * target, int hitChance, bool isFrozen)
 : Character{initHP, initAtk, initDef, Gold, row, col}, Target(target), hitChance{hitChance}, isFrozon{isFrozen} {}
 
+void Enemy::changeFrozen() {
+    isFrozen = !isFrozen;
+}
+
+void Enemy::hurt(PC *p) {
+    int effect = p->damage(p);
+    changeHP(effect);
+}
+
 void Elf::attack(PC * player) {
     player->hurt(*this);
 }
