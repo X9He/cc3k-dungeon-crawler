bb59c17c31f7e5e1a13b095e49d972267f4e370c
wxli0 <w328li@uwaterloo.ca>
2017-03-27 15:16:06 -0400

    Update Potion.cc

--- Potion.cc
+++ Potion.cc
@@ -1,49 +1,42 @@
-//
-//  potion.cpp
-//  CC3K
-//
-//  Created by Li on 2017-03-25.
-//  Copyright © 2017 Wanxin Li. All rights reserved.
-//
 
 #include "Potion.h"
 
-void RH::useItem() {
+void RH::useItem(int mag) {
     int max_HP = target->getmax();
     int cur_HP = target->getHP();
     
-    if (cur_HP+10 <= max_HP) {
-        target->changeHP(cur_HP+10);
+    if (cur_HP+mag*10 <= max_HP) {
+        target->changeHP(cur_HP+mag*10);
     } else {
         target->changeHP(max_HP);
     }
 }
 
-void BA::useItem() {
+void BA::useItem(int mag) {
     int cur_Atk = target->getAtk();
-    target->changeAtk(cur_Atk+5);
+    target->changeAtk(cur_Atk+mag*5);
 }
 
-void BD::useItem() {
+void BD::useItem(int mag) {
     int cur_Def = target->getDef();
-    target->changeDef(cur_Def+5);
+    target->changeDef(cur_Def+mag*5);
 }
 
-void PH::useItem() {
+void PH::useItem(int mag) {
     int cur_HP = target->getHP();
-    if (cur_HP-10 >= 0) {
-        target->changeHP(cur_HP-10);
+    if (cur_HP-mag*10 >= 0) {
+        target->changeHP(cur_HP-mag*10);
     } else {
         target->changeHP(0);
     }
 }
 
-void WA::useItem() {
+void WA::useItem(int mag) {
     int cur_Atk = target->getAtk();
-    target->changeAtk(cur_Atk-5);
+    target->changeAtk(cur_Atk-mag*5);
 }
 
-void WD::useItem() {
+void WD::useItem(int mag) {
     int cur_Def = target->getDef();
-    target->changeAtk(cur_Def-5);
+    target->changeAtk(cur_Def-mag*5);
 }
