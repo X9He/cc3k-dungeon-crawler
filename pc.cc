abcee9ff417813e497197f609110e118c419e194
wxli0 <w328li@uwaterloo.ca>
2017-03-27 18:38:08 -0400

    Update pc.cc

--- pc.cc
+++ pc.cc
@@ -52,3 +52,43 @@ void PC::hurt(Orcs &o){
   o.changetHP(effet);
 }
   
+//
+void PC::hurt(Merchant & m) {
+    damage(m);
+}
+
+void PC::hurt(Dragon & d) {
+    damage(d);
+}
+
+void PC::hurt(Elf & e) {
+    damage(e, 2);
+}
+
+void PC::hurt(Dwarf & d) {
+    damage(d);
+}
+
+void PC::hurt(Halfing & h) {
+    damage(h);
+}
+
+void PC::hurt(Orcs & o) {
+    damage(o);
+}
+
+void PC::hurt(Human & h) {
+    damage(h);
+}
+
+void Vampire::hurt(Elf & e) {
+    damage(e);
+}
+
+void Goblin::hurt(Orcs & o) {
+    damage(o, 1.5);
+}
+
+void Drow::hurt(Elf & e) {
+    damage(e);
+}
