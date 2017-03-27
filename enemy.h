33c3083b79d121b1b382e5c61375f3a029dcd247
wxli0 <w328li@uwaterloo.ca>
2017-03-27 16:18:51 -0400

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -25,21 +25,25 @@ class NormalEnemy: public Enemy {
 class Elf: public NormalEnemy {
 public:
     void attack(PC *) override;
+    Elf(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen);
 };
 
 class Dward: public NormalEnemy {
 public:
     void attack(PC *) override;
+    Dward(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen);
 };
 
 class Halfing: public NormalEnemy {
 public:
     void attack(PC *) override;
+    Halfing(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen);
 };
 
 class Orcs: public NormalEnemy {
 public:
     void attack(PC *) override;
+    Orcs(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen);
 };
 
 class Merchant: public Enemy {
@@ -49,17 +53,20 @@ public:
     void attack(PC *) override;
     bool IsHostile();
     void changeStatus();
+    Merchant(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen);
 };
 
 class Dragon: public Enemy {
     Treasure * hoard;
 public:
     void attack (PC *) override;
+    Dragon(int row, int col, PC * target, int hitChance, bool isFrozen, Treasure * t);
 };
 
 class Human: public Enemy {
 public:
     void attack (PC *) override;
+    Human(int row, int col, PC * target, int hitChance, bool isFrozen);
 };
 
 #endif /* enemy_hpp */
