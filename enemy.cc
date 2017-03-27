c1c62b9e87f0858fb9f2b33c1806a2201b99465b
wxli0 <w328li@uwaterloo.ca>
2017-03-27 16:15:56 -0400

    Update enemy.cc

--- enemy.cc
+++ enemy.cc
@@ -12,8 +12,26 @@ void Elf::attack(PC * player) {
     player->hurt(*this);
 }
 
-Elf(int row, int col, PC * target, int hitChance, bool isFrozen):
+Human(int row, int col, PC * target, int hitChance, bool isFrozen):
 Enemy{140, 20, 20, 4, row, col, PC, hitChance, isFrozen}{}
+
+Dward(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen):
+Enemy{100, 20, 30, Gold, row, col, PC, hitChance, isFrozen}{}
+
+Elf(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen):
+Enemy{140, 30, 10, Gold, row, col, PC, hitChance, isFrozen}{}
+
+Orcs(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen):
+Enemy{180, 30, 25, Gold, row, col, PC, hitChance, isFrozen}{}
+
+Merchant(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen):
+Enemy{30, 70, 5, Gold, row, col, PC, hitChance, isFrozen}{}
+
+Dragon(int row, int col, PC * target, int hitChance, bool isFrozen, Treasure * t):
+Enemy{150, 20, 20, 6, row, col, PC, hitChance, isFrozen}, Treasure{t}{}
+
+Halfing(int row, int col, PC * target, int Gold, int hitChance, bool isFrozen):
+Enemy{100, 15, 20, Gold, row, col, PC, hitChance, isFrozen}{}
 
 void Dward::attack(PC * player) {
     player->hurt(*this);
