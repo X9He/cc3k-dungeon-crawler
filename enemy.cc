e2ecd901f52ff2a8e40f6a2957b2cea2f0a3ef30
wxli0 <w328li@uwaterloo.ca>
2017-03-27 16:04:47 -0400

    Update enemy.cc

--- enemy.cc
+++ enemy.cc
@@ -5,12 +5,15 @@
 // enemy attack 50% miss
 
 
-Enemy::Enemy(int initHP, int initAtk, int initDef, int Gold, int row, int col, int dropAmount, PC * target, int hitChance, bool isFrozen)
-: Character{initHP, initAtk, initDef, Gold, row, col}, dropAmount(dropAmount), Target(target), hitChance{hitChance}, isFrozon{isFrozen} {}
+Enemy::Enemy(int initHP, int initAtk, int initDef, int Gold, int row, int col, PC * target, int hitChance, bool isFrozen)
+: Character{initHP, initAtk, initDef, Gold, row, col}, Target(target), hitChance{hitChance}, isFrozon{isFrozen} {}
 
 void Elf::attack(PC * player) {
     player->hurt(*this);
 }
+
+Elf(int row, int col, PC * target, int hitChance, bool isFrozen):
+Enemy{140, 20, 20, 4, row, col, PC, hitChance, isFrozen}{}
 
 void Dward::attack(PC * player) {
     player->hurt(*this);
