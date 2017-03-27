4cc255b2f63add102be5f4a94a62a56f4db81f3e
wxli0 <w328li@uwaterloo.ca>
2017-03-26 22:43:44 -0400

    Add files via upload

---
+++ enemy.hpp
@@ -0,0 +1,73 @@
+//
+//  enemy.hpp
+//  CC3K
+//
+//  Created by Li on 2017-03-26.
+//  Copyright © 2017 Wanxin Li. All rights reserved.
+//
+
+#ifndef enemy_hpp
+#define enemy_hpp
+
+class Character;
+class PC;
+
+class Enemy: public Character {
+    int dropAmount;
+    PC * Target;
+    int hitChance;
+    bool isFrozon;
+    
+public:
+    void move();
+    void virtual attack(PC *) = 0;
+    void HPchange();
+    Enemy(int, int, int, int, int, int, int, PC *, int, bool);
+};
+
+class NormalEnemy: public Enemy {
+    bool IsHostile;
+    virtual void attack (PC *) = 0;
+};
+
+class Elf: public NormalEnemy {
+public:
+    void attack(PC *) override;
+};
+
+class Dward: public NormalEnemy {
+public:
+    void attack(PC *) override;
+};
+
+class Halfing: public NormalEnemy {
+public:
+    void attack(PC *) override;
+};
+
+class Orcs: public NormalEnemy {
+public:
+    void attack(PC *) override;
+};
+
+class Merchant: public Enemy {
+    bool status;
+    vector <Item *> store;
+public:
+    void attack(PC *) override;
+    bool IsHostile();
+    void changeStatus();
+};
+
+class Dragon: public Enemy {
+    Treasure * hoard;
+public:
+    void attack (PC *) override;
+};
+
+class Human: public Enemy {
+public:
+    void attack (PC *) override;
+};
+
+#endif /* enemy_hpp */
