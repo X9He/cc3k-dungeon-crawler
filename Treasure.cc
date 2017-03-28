b213de87404c4360b495b00eb833c9fe49fb83d4
wxli0 <w328li@uwaterloo.ca>
2017-03-27 22:37:40 -0400

    Update Treasure.cc

--- Treasure.cc
+++ Treasure.cc
@@ -2,6 +2,16 @@
 
 #include "Treasure.h"
 
+int random(){
+    int x = 0;
+    int y = 1;
+    int ran;
+    srand(time(0));
+    ran = x + (rand() % (y - x + 1));
+    if (ran == 0) return 1;
+    return 2;
+}
+
 Treasure::Treasure(int value, bool protect):
 value{value}, protect{protect} {}
 
