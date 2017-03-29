c696d192f69d00052072907349a41d6960d022ef
wxli0 <w328li@uwaterloo.ca>
2017-03-29 16:37:39 -0400

    Add files via upload

--- enemy.h
+++ enemy.h
@@ -4,74 +4,94 @@
 
 #include "pc.h"
 #include "character.h"
+#include "treasure.h"
+#include <vector>
 
+class Troll;
+class Vampire;
+class Goblin;
+class Drow;
+class Elf;
 
 class Enemy: public Character {
     PC * Target;
-    bool isFrozon;
+    bool isFrozen;
     bool moved;
 public:
     ~Enemy();
-    Enemy(int initHP, int intAtk, int initDef, int Gold, PC * target, bool isFrozen = false, bool moved = false} 
+    Enemy(int initHP, int intAtk, int initDef, int Gold, PC * target, bool isFrozen = false, bool moved = false); 
     void move();
-    virtual void attack(PC *) = 0;
-    Enemy(PC *);
+    virtual void attack(PC *target) = 0;
+    Enemy(PC *target);
     void changeFrozen();
     void changeMoved();
     bool getMoved();
+    virtual void hurt(Troll *p);
+    virtual void hurt(Vampire *p);
+    virtual void hurt(Goblin *p);
+    virtual void hurt(Drow *p);
+    virtual void hurt(PC *p);
 };
 
 class NormalEnemy: public Enemy {
-    bool isHostile;
-    virtual void attack (PC *) = 0;
+    bool IsHostile;
+    virtual void attack (PC *target) = 0;
+public:
+    NormalEnemy(int initHP, int intAtk, int initDef, int Gold, PC * target, bool isFrozen = false, bool moved = false);
 };
 
 class Elf: public NormalEnemy {
 public:
-    void attack(PC *) override;
-    Elf(PC *);
+    void attack(PC *target) override;
+    Elf(PC *target);
 };
 
-class Dward: public NormalEnemy {
+class Dwarf: public NormalEnemy {
 public:
-    void attack(PC *) override;
-    Dward(PC *);
+    void attack(PC *target) override;
+    Dwarf(PC *target);
 };
 
-class Halfing: public NormalEnemy {
+class Halfling: public NormalEnemy {
 public:
-    void attack(PC *) override;
-    Halfing(PC *);
+    void attack(PC *target) override;
+    Halfling(PC *target);
+    void hurt(Troll *p) override;
+    void hurt(Drow *p) override;
+    void hurt(Vampire *p) override;
+    void hurt(PC *p) override;
+    void hurt(Goblin *p) override;
+       
 };
 
 class Orcs: public NormalEnemy {
 public:
-    void attack(PC *) override;
-    Orcs(PC *);
+    void attack(PC *target) override;
+    Orcs(PC *targte);
 };
 
 class Merchant: public Enemy {
     bool status;
-    vector <Item *> store;
+    std::vector<Item *> store;
 public:
-    void attack(PC *) override;
+    void attack(PC *target) override;
     bool IsHostile();
     void changeStatus();
-    Merchant(PC *);
+    Merchant(PC *target);
 };
 
 class Dragon: public Enemy {
     Treasure * hoard;
 public:
-    void attack (PC *) override;
+    void attack (PC *targte) override;
     Treasure * getHoard();
     Dragon(PC * target, Treasure * t);
 };
 
 class Human: public Enemy {
 public:
-    void attack (PC *) override;
-    Human(PC *);
+    void attack (PC *target) override;
+    Human(PC *target);
 };
 
 #endif /* enemy_hpp */
